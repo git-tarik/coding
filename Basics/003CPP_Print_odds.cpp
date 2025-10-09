b331fa57a0953464916d9342fbd15cbdcec35e31
Md Tarik Anvar <tarik.22190503033@cuj.ac.in>
2025-10-09 23:38:05 +0530

    Add files via upload

---
+++ Basics/003CPP_Print_odds.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;   
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Odd numbers from 1 to " << n << " are:" << endl;
+    for (int i = 1; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
